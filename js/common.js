5246b483a37cc2df33d47fd5d68a8128e57abed4
SpokBATMAN <game1life1@gmail.com>
2017-02-12 01:21:15 +0200

    344
    24324

--- js/common.js
+++ js/common.js
@@ -65,3 +65,45 @@ $(document).ready(function(){
             }
         });//scroll
     });
+
+/*var bg = [
+    '../img/header-1.jpg',
+    '../img/header-3.jpg',
+    '../img/header-4.jpg' 
+]
+var count =1;
+$(document).ready(function(){
+    if(count<=3){
+        setInterval(function() {
+   $('header').css({'background':'url('+bg[count]+')'});
+count++;
+        }, 2000);
+    }
+    count=0;
+});*/
+
+var imgHead = [
+			'../img/header-1.jpg',
+    '../img/header-3.jpg',
+    '../img/header-4.jpg' 
+		], i=1;
+	function csaHead(){
+
+		if(i > (imgHead.length-1)){
+			$('header').animate({'opacity':'1'},0,function(){
+				i=1;
+				$('header').css({'background':'url('+imgHead[0]+')'});
+			});
+			
+		}else{
+			$('header').animate({'opacity':'1'},0,function(){
+				$('header').css({'background':'url('+imgHead[i]+')'});
+				i++;
+			});
+		}
+		
+	}
+	var intervalCsaHead = setInterval(csaHead,4000);
+
+
+
