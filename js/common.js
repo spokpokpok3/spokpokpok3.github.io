e6edaf371e639d8fda5da942fee80793c9a42167
SpokBATMAN <game1life1@gmail.com>
2017-02-20 20:30:46 +0200

    выф
    вфы

--- js/common.js
+++ js/common.js
@@ -114,3 +114,35 @@ $('.slider').slick({
     // instead of a settings object
   ]
 });
+
+
+/*button scroll*/
+$(document).ready(function () {
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $(".button-scroll").addClass("button-scroll-display");
+           
+        }
+        else{
+            $(".button-scroll").removeClass("button-scroll-display");
+        }
+    });
+});
+
+/*scroll to top*/
+$(document).ready(function(){
+	$("body").on("click",".button-scroll", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
